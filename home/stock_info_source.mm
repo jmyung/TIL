fc54f86581108a7e61b3b79ace41d12f9f22e30d
DESKTOP-HNB57EC\verys <verystrongjoe@gmail.com>
2017-05-07 23:34:11 +0900

    add home directory and add something done in starbuck

---
+++ home/stock_info_source.mm
@@ -0,0 +1,15 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1494140362766" ID="ID_502228232" MODIFIED="1494153653127" TEXT="&#xc8fc;&#xc2dd; &#xb370;&#xc774;&#xd130;">
+<node CREATED="1494140584523" ID="ID_157158883" LINK="http://financialfreedom.kr/834-2/" MODIFIED="1494140595039" POSITION="right" TEXT="&#xad6c;&#xae00; &#xd30c;&#xc774;&#xb0b8;&#xc2a4; &#xac15;&#xc758;">
+<node CREATED="1494142466194" ID="ID_1181885301" LINK="http://financialfreedom.kr/834-2/" MODIFIED="1494142467352" TEXT="1"/>
+</node>
+<node CREATED="1494145527166" ID="ID_879143747" MODIFIED="1494145753616" POSITION="left" TEXT="Yahoo Finance">
+<node CREATED="1494145755803" ID="ID_416879490" LINK="http://investexcel.net/all-yahoo-finance-stock-tickers/" MODIFIED="1494145760100" TEXT="Tickers List"/>
+</node>
+<node CREATED="1494146839835" ID="ID_1505065951" LINK="http://www.kosdaq.or.kr/disclosureinfo/dividendinfo.do?method=searchDividendInfoMain" MODIFIED="1494146842785" POSITION="right" TEXT="&#xbc30;&#xb2f9;&#xc815;&#xbcf4;"/>
+<node CREATED="1494157206315" ID="ID_1487768228" MODIFIED="1494157213655" POSITION="left" TEXT="Google Finance">
+<node CREATED="1494157217059" ID="ID_1510406912" LINK="https://support.google.com/docs/table/25273?hl=ko&amp;ref_topic=3105411&amp;vid=1-635773048177215665-1964767047" MODIFIED="1494157220122" TEXT="Sheet Function"/>
+</node>
+</node>
+</map>
